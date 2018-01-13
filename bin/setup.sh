53523725f8ccddd666ce758e1b34980264425fb1
Serge Koba <s.koba@mobidev.biz>
2018-01-13 21:35:59 +0200

    optimization

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker-compose run action-service bundle
-docker-compose run action-service ./cli db_migrate
+docker-compose run action-service bundle exec ./cli db_migrate
 cd ../action-service/docs && mkdocs build # build api doc
